415123e1e965f8a712bbcc5487edb22688290b30
fupeng <pengf@opera.com>
2018-12-12 22:56:26 +0800

    remove unuseful embedding files

---
+++ word2vec/test_embedding.py
@@ -0,0 +1,11 @@
+from gensim.models.keyedvectors import KeyedVectors
+from gensim.test.utils import datapath
+from pathlib import Path
+
+model = KeyedVectors.load_word2vec_format(Path.cwd() / 'model_test.txt', binary=False)
+
+print('女生')
+print(model.most_similar(positive='女生'))
+print('----------------')
+print('中国')
+print(model.most_similar(positive='中国'))
